0282516a69072db25728229902a3373cd693f6d7
yassine-akazi <akazi.yassine@gmail.com>
2022-12-22 12:11:38 +0100

    first

---
+++ Module/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    
+})
+
+module.exports = mongoose.model('User', User)  
---
+++ server.js
@@ -0,0 +1,104 @@
+const { response } = require("express");
+const express = require("express")
+const app = express()
+const User = require("./Module/User");
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+app.set("view engine", "ejs")
+
+app.use(express.static("static"))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.get("/", function (req, res) {
+    res.render("index")
+    console.log('index is working')
+
+
+})
+app.get("/nun", function (req, res) {
+    res.render("nun")
+    console.log('login is working')
+})
+
+app.get("/login", function (req, res) {
+    res.render("login")
+    console.log('login is working')
+})
+
+app.post('/login', async function(req, res) {
+    const { email, password } = req.body ;
+    const user = await User.findOne({ email })
+
+    if(!user){
+        res.status(400).redirect("nun")
+        return;
+    }
+
+    if(password !== user.password){
+        res.status(400).redirect("nun")
+        return;
+    }
+
+    const token = jwt.sign({ user }, "Super-secret", {
+        expiresIn: "1d"
+    })
+
+    res.redirect(`tmp?token=${token}`, 301)
+})
+
+app.get("/tmp", function (req, res) {
+    res.render("tmp")
+    console.log('tmp is working')
+
+})
+
+app.get("/tmp1", function (req, res) {
+    res.render("tmp1")
+    console.log('tmp1 is working')
+
+
+})
+
+app.get("/tmp2", function (req, res) {
+    res.render("tmp2")
+    console.log('tmp2 is working')
+
+
+})
+
+app.get("/sing-up", async function (req, res) {
+    res.render("sing-up")
+    console.log('sing-up is working')
+
+})
+
+app.post('/sing-up', async function (req, res){
+    const { name, email, password } = req.body;
+    await User.create({
+      name,
+      email, 
+      password
+    })
+    res.redirect("login", 301)
+})
+
+
+
+
+
+app.listen(8000, function () {
+    console.log('App running on http://localhost:8000')
+})
+
+const uri = "mongodb+srv://yassine:yassine@cluster0.zx6bz03.mongodb.net/resume-db?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', true)
+mongoose.connect(uri, {}, (err, data) => {
+    if(err) throw err
+    console.log("Connected to Mongo DB")
+})
+
